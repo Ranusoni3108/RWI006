85265a023251f3fd8bd0b5f9b53427c3f191e4e5
Ranusoni3108 <165899991+Ranusoni3108@users.noreply.github.com>
2024-05-02 21:50:43 +0530

    Add files via upload

---
+++ joinMethod.java
@@ -0,0 +1,45 @@
+public class joinMethod extends Thread
+{
+	public void run()
+	{
+		try
+		{
+		for(int i=1; i<=5; i++)
+		{
+			System.out.println("child thread : "+i);
+			Thread.sleep(1000);
+			
+		}
+	
+	}
+	catch(Exception e)
+	{
+		System.out.println(e);
+	}
+	}
+
+	public static void main(String[] args)throws InterruptedException
+	{
+		joinMethod t1=new joinMethod();
+		t1.start();
+		
+		t1.join();
+		
+		try
+		{
+		for(int i=1; i<=5; i++)
+		{
+			System.out.println("main thread : "+i);
+			Thread.sleep(1000);
+			
+		}
+	
+	}
+	catch(Exception e)
+	{
+		System.out.println(e);
+		}
+
+	}
+
+}
